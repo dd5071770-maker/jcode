9ec14d22597b495d09d4b0ab8130502bc82b4112
dd5071770-maker <dd5071770@gmail.com>
2026-04-25 11:02:45 +0530

    Added greatest of three numbers program

---
+++ three.java
@@ -0,0 +1,9 @@
+public class three {
+    public static void main(String[] args) {
+        int a = 10, b = 25, c = 15;
+
+        int greatest = Math.max(a, Math.max(b, c));
+
+        System.out.println("Greatest number is: " + greatest);
+    }
+}
